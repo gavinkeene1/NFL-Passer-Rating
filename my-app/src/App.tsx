6613fb5587ad425781e074ca3a7006a242c26d74
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2023-01-29 20:51:25 +0800

    Add Calculated Passer Rating Column
    TODO: Fix Total Row Passer Rating

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,55 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const App: React.FC = () => {
-  const [week, setWeek] = useState('');
+  const [week, setWeek] = useState("");
   const [attempts, setAttempts] = useState(0);
   const [completions, setCompletions] = useState(0);
   const [yards, setYards] = useState(0);
   const [touchdowns, setTouchdowns] = useState(0);
   const [interceptions, setInterceptions] = useState(0);
-  const [stats, setStats] = useState<Array<{
-    week: string,
-    attempts: number | undefined;
-    completions: number | undefined;
-    yards: number | undefined;
-    touchdowns: number;
-    interceptions: number;
-  }>>([]);
+  const [rating, setRating] = useState(0);
+  const [stats, setStats] = useState<
+    Array<{
+      week: string;
+      attempts: number;
+      completions: number;
+      yards: number;
+      touchdowns: number;
+      interceptions: number;
+      rating: number;
+    }>
+  >([]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     switch (event.target.name) {
-      case 'week':
+      case "week":
         setWeek(event.target.value);
         break;
-      case 'attempts':
+      case "attempts":
         setAttempts(parseInt(event.target.value, 10));
         break;
-      case 'completions':
+      case "completions":
         setCompletions(parseInt(event.target.value, 10));
         break;
-      case 'yards':
+      case "yards":
         setYards(parseInt(event.target.value, 10));
         break;
-      case 'touchdowns':
+      case "touchdowns":
         setTouchdowns(parseInt(event.target.value, 10));
         break;
-      case 'interceptions':
+      case "interceptions":
         setInterceptions(parseInt(event.target.value, 10));
         break;
       default:
         break;
     }
+    setRating(getPasserRating(
+      completionPercentage,
+      yardsPerAttempt,
+      touchdownPercentage,
+      interceptionPercentage
+    ));
+    console.log(`just tried to get rating - it is ${rating}`);
   };
 
   const addStats = () => {
-    setStats([...stats, {
-      week,
-      attempts,
-      completions,
-      yards,
-      touchdowns,
-      interceptions,
-    }]);
+    setStats([
+      ...stats,
+      {
+        week,
+        attempts,
+        completions,
+        yards,
+        touchdowns,
+        interceptions,
+        rating,
+      },
+    ]);
+    setRating(rating);
     setAttempts(0);
     setCompletions(0);
     setYards(0);
@@ -57,11 +73,71 @@ export const App: React.FC = () => {
     setInterceptions(0);
   };
 
+  // Function to calculate completion percentage
+  const getCompletionPercentage = (
+    completions: number,
+    attempts: number
+  ): number => {
+    return (completions / attempts) * 100;
+  };
+
+  // Function to calculate yards per attempt
+  const getYardsPerAttempt = (yards: number, attempts: number): number => {
+    return yards / attempts;
+  };
+
+  // Function to calculate touchdown percentage
+  const getTouchdownPercentage = (
+    touchdowns: number,
+    attempts: number
+  ): number => {
+    return (touchdowns / attempts) * 100;
+  };
+
+  // Function to calculate interception percentage
+  const getInterceptionPercentage = (
+    interceptions: number,
+    attempts: number
+  ): number => {
+    return (interceptions / attempts) * 100;
+  };
+
+  // Function to calculate passer rating
+  const getPasserRating = (
+    completionPercentage: number,
+    yardsPerAttempt: number,
+    touchdownPercentage: number,
+    interceptionPercentage: number
+  ): number => {
+    const a = (completionPercentage - 30) / 20;
+    const b = (yardsPerAttempt - 3) / 4;
+    const c = (touchdownPercentage - 3) / 4;
+    const d = 2.375 - interceptionPercentage / 2;
+
+    return ((a + b + c + d) / 6) * 100;
+  };
+
+  const completionPercentage = getCompletionPercentage(completions, attempts);
+  const yardsPerAttempt = getYardsPerAttempt(yards, attempts);
+  const touchdownPercentage = getTouchdownPercentage(touchdowns, attempts);
+  const interceptionPercentage = getInterceptionPercentage(
+    interceptions,
+    attempts
+  );
+  const passerRating = getPasserRating(
+    completionPercentage,
+    yardsPerAttempt,
+    touchdownPercentage,
+    interceptionPercentage
+  );
+
+  console.log(`passerRating is ${passerRating}`);
+
   return (
     <div>
       <h1>NFL Passer Rating Calculator</h1>
       <form>
-      <label htmlFor="attempts">Season/Week:</label>
+        <label htmlFor="attempts">Season/Week:</label>
         <input
           type="string"
           id="week"
@@ -107,73 +183,74 @@ export const App: React.FC = () => {
         />
         <br />
         <label htmlFor="interceptions">Interceptions:</label>
-<input
-       type="number"
-       id="interceptions"
-       name="interceptions"
-       onChange={handleInputChange}
-     />
-</form>
-<button onClick={addStats}>Add Stats</button>
-<table>
-<thead>
-<tr>
-<th>Season/Week</th>
-<th>Attempts</th>
-<th>Completions</th>
-<th>Yards</th>
-<th>Touchdowns</th>
-<th>Interceptions</th>
-</tr>
-</thead>
-<tbody>
-{stats.map((stat, index) => (
-<tr key={index}>
-<td>{stat.week}</td>
-<td>{stat.attempts}</td>
-<td>{stat.completions}</td>
-<td>{stat.yards}</td>
-<td>{stat.touchdowns}</td>
-<td>{stat.interceptions}</td>
-</tr>
-))}
-<tr>
-<td>
-{`${stats.length} Games`}
-</td>
-<td>
-{stats.reduce(
-(acc, stat) => acc + stat.attempts!, // TODO: Handle "possibly undefined"
-0
-)}
-</td>
-<td>
-{stats.reduce(
-(acc, stat) => acc + stat.completions!, // TODO: Handle "possibly undefined"
-0
-)}
-</td>
-<td>
-{stats.reduce(
-(acc, stat) => acc + stat.yards!, // TODO: Handle "possibly undefined"
-0
-)}
-</td>
-<td>
-{stats.reduce(
-(acc, stat) => acc + stat.touchdowns,
-0
-)}
-</td>
-<td>
-{stats.reduce(
-(acc, stat) => acc + stat.interceptions,
-0
-)}
-</td>
-</tr>
-</tbody>
-</table>
-</div>
-);
+        <input
+          type="number"
+          id="interceptions"
+          name="interceptions"
+          onChange={handleInputChange}
+          value={interceptions}
+        />
+        <br />
+        <label htmlFor="rating">Passer Rating:</label>
+        <input
+          disabled={true}
+          type="number"
+          id="rating"
+          name="rating"
+          value={rating}
+        />
+      </form>
+      <button onClick={addStats}>Add Stats</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Season/Week</th>
+            <th>Attempts</th>
+            <th>Completions</th>
+            <th>Yards</th>
+            <th>Touchdowns</th>
+            <th>Interceptions</th>
+            <th>Passer Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stats.map((stat, index) => (
+            <tr key={index}>
+              <td>{stat.week}</td>
+              <td>{stat.attempts}</td>
+              <td>{stat.completions}</td>
+              <td>{stat.yards}</td>
+              <td>{stat.touchdowns}</td>
+              <td>{stat.interceptions}</td>
+              <td>{stat.rating}</td>
+            </tr>
+          ))}
+          <tr>
+            <td>{`${stats.length} Games`}</td>
+            <td>
+              {stats.reduce(
+                (acc, stat) => acc + stat.attempts!, // TODO: Handle "possibly undefined"
+                0
+              )}
+            </td>
+            <td>
+              {stats.reduce(
+                (acc, stat) => acc + stat.completions!, // TODO: Handle "possibly undefined"
+                0
+              )}
+            </td>
+            <td>
+              {stats.reduce(
+                (acc, stat) => acc + stat.yards!, // TODO: Handle "possibly undefined"
+                0
+              )}
+            </td>
+            <td>{stats.reduce((acc, stat) => acc + stat.touchdowns, 0)}</td>
+            <td>{stats.reduce((acc, stat) => acc + stat.interceptions, 0)}</td>
+            <td>{stats.reduce((acc, stat) => stat.rating, 0)}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
